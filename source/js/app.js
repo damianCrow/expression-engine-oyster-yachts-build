5da323c5bfdc10883cd4e03fc977ee74fa5e4cfa
Chris Macdonald <chris.m@interstateteam.com>
2016-02-25 16:13:56 +0000

    bourbon fix

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+
+$(function() {
+
+	// Yacht nav - scroll to section
+	$('#yacht-nav').on('click', '.scroll', function(e) {
+		e.preventDefault();
+
+		$('html, body').animate({scrollTop:$($(this).attr('href')).position().top}, 700);
+	});
+
+
+	$('#layout-slider').cycle({
+		slides: '> div',
+		paused: true,
+		pager: '.slider-pager',
+		pagerTemplate: ''
+	});
+});
