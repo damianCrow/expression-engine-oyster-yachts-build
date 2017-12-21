aba3d9ccdf6d5d06462363a5854db6aa660c8333
Damian Whyte <damian.w@interstateteam.com>
2017-12-21 12:03:59 +0000

    projects team

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -32247,7 +32247,7 @@ return jQuery;
 }));
 
 },{"../fix-ios-sizes/fix-ios-sizes":365,"lazysizes":364}],370:[function(require,module,exports){
-/*! lightgallery - v1.6.6 - 2017-12-20
+/*! lightgallery - v1.6.5 - 2017-11-16
 * http://sachinchoolur.github.io/lightGallery/
 * Copyright (c) 2017 Sachin N; Licensed GPLv3 */
 (function (root, factory) {
@@ -33731,7 +33731,7 @@ return jQuery;
 }));
 
 },{"jquery":363}],372:[function(require,module,exports){
-/*! lg-hash - v1.0.4 - 2017-12-20
+/*! lg-hash - v1.0.3 - 2017-10-19
 * http://sachinchoolur.github.io/lightGallery
 * Copyright (c) 2017 Sachin N; Licensed GPLv3 */
 
@@ -33780,7 +33780,7 @@ return jQuery;
         // Change hash value on after each slide transition
         _this.core.$el.on('onAfterSlide.lg.tm', function(event, prevIndex, index) {
             if (history.replaceState) {
-                history.replaceState(null, null, window.location.pathname + window.location.search + '#lg=' + _this.core.s.galleryId + '&slide=' + index);
+                history.replaceState(null, null, window.location.pathname + '#lg=' + _this.core.s.galleryId + '&slide=' + index);
             } else {
                 window.location.hash = 'lg=' + _this.core.s.galleryId + '&slide=' + index;
             }
